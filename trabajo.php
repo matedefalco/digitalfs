7bbfe6b29d2f54f22b38f5af1e88fda8219627e4
Mateo <mateodefalco1@gmail.com>
2019-09-10 17:52:09 -0300

    base

---
+++ trabajo.php
@@ -0,0 +1,67 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <link rel="stylesheet" href="css/trabajo.css">
+    <title>Trabajo Final</title>
+  </head>
+  <body>
+    <header>
+      <nav clas="header">
+        <input class="lupa" type="search" name="lupa" value="">
+        <a class="login" href="login.php">
+          <button type="button" class="btn btn-primary">LOGIN</button>
+        </a>
+      </nav>
+    </header>
+    <main>
+      <nav class="post">
+        <nav class="user">
+          <nav class="user_img">
+            <a href="#">
+              <img src="" alt="">
+            </a>
+          </nav>
+          <nav class="user_name">
+            <a href="#">
+              USER NAME
+              <?php  ?>
+            </a>
+          </nav>
+        </nav>
+        <nav class="main_img">
+          <img src="" alt="">
+        </nav>
+        <nav class="interaction">
+          <ul>
+            <li class="up">UP</li>
+            <li class="down">DOWN</li>
+            <li class="share">SHARE</li>
+            <li class="options">OPTIONS</li>
+          </ul>
+        </nav>
+      </nav>
+    </main>
+    <footer>
+      <ul>
+        <li class="home"></li>
+        <li class="stock"></li>
+        <li class="add"></li>
+        <li class="chat"></li>
+        <li class="profile"></li>
+      </ul>
+    </footer>
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+
+
+  </body>
+</html>
