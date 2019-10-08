196f3068a83b099a9e0a1737e35a05a2a27a2588
Mateo <mateodefalco1@gmail.com>
2019-10-07 23:18:40 -0300

    comienzo página user

---
+++ user.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link rel="stylesheet" href="css/user.css">
+    <title>User</title>
+  </head>
+  <body>
+    <header>
+      <button type="button" name="button"></button>
+    </header>
+    <main>
+      <section>
+        <img src="" alt="">
+        <?php
+        // nombre usuario
+         ?>
+      </section>
+      <div class="gallery">
+        <?php
+        // images
+         ?>
+      </div>
+    </main>
+    <footer class="">
+      <ul class="nav_final">
+        <li class="barra home">
+          <a href="#"><i class="fa fa-home"></i></a>
+        </li>
+        <li class="barra stock">
+          <a href="#"><i class="fa fa-image"></i></a>
+        </li>
+        <li class="barra add">
+          <a href="#"><i class="fa fa-plus-circle"></i></a>
+        </li>
+        <li class="barra chat">
+          <a href="#"><i class="fa fa-comment"></i></a>
+        </li>
+        <li class="barra profile">
+          <a href="#"><i class="fa fa-user"></i></a>
+        </li>
+      </ul>
+    </footer>
+  </body>
+</html>
