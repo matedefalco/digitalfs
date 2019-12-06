c2860d3d8c5cf5f0e5662e4de17a9d41ecaef73b
Juan Pablo Rodriguez Acevedo <juanpi999@hotmail.com>
2019-12-06 14:47:16 -0300

    css 3

--- 11armo/laravelPI/resources/views/layouts/app.blade.php
+++ 11armo/laravelPI/resources/views/layouts/app.blade.php
@@ -23,9 +23,10 @@
 </head>
 <body>
     <div id="app">
-        <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
+        <nav class="mainnav navbar navbar-expand-md navbar-light shadow-sm">
             <div class="container">
                 <a class="navbar-brand" href="/">
+                    Home
                 </a>
                 <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
                     <span class="navbar-toggler-icon"></span>
