cb665ab0b3a3bb6b3bb2d6831c447e29ab8cc86f
Mateo <mateodefalco1@gmail.com>
2019-12-06 16:57:14 -0300

    ee

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -79,11 +79,11 @@
           </div>
         <main/>
         <footer class="bottom_nav container col-12" align="center">
-              <a href="#"><i class="barra fa fa-home fa-3x col-2"></i></a>
+              <a href="/"><i class="barra fa fa-home fa-3x col-2"></i></a>
               <a href="#"><i class="barra fa fa-image fa-3x col-2"></i></a>
               <a href="/crearPost"><i class="barra fa fa-plus-circle fa-3x col-2"></i></a>
               <a href="#"><i class="barra fa fa-comment fa-3x col-2"></i></a>
-              <a href="/user/"{{$currentUser->id}}><i class="barra fa fa-user fa-3x col-2"></i></a>
+              <a href="/user/"{{--}}{{$currentUser->id}}--}}><i class="barra fa fa-user fa-3x col-2"></i></a>
         </footer>
     </div>
 </body>
