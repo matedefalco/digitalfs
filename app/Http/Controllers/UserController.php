0f5f3cdcdf0ffc3c7e328524f3f3c9f670082060
Mateo <mateodefalco1@gmail.com>
2019-12-17 16:27:22 -0300

    nbvcx

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -11,7 +11,7 @@ class UserController extends Controller
   public function index($id)
   {
     $posts = Post::where("user_id", "=", $id )->get();
-    $user = User:where('id', '=',$id);
+    $user = User::where('id', '=',$id)->get();
     return view('user', compact('posts','user'));
   }
 }
