82cc781f677b2c60b32dcdf99ede5e52c37b8fbb
Mateo <mateodefalco1@gmail.com>
2019-12-17 16:02:56 -0300

    just

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -2,7 +2,7 @@
 @section('content')
       <div class="content">
           <div class="title m-b-md">
-            <h1 class="welcome">WELCOME TO LENTI</h1>
+            <h1 class="welcome text-justify">WELCOME TO LENTI</h1>
             <h2>A website for photographers to show their art</h2>
             <h3>Get into your account now and enjoy!!</h3>
           </div>
