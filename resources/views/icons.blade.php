6b6738b84a33d36fa3602dc643c40e6f4a04404d
Mateo <mateodefalco1@gmail.com>
2019-12-17 15:18:08 -0300

    aa

--- resources/views/icons.blade.php
+++ resources/views/icons.blade.php
@@ -7,11 +7,15 @@
     <i class="likesitem la la-thumbs-down fa-2x" style="width:100%"></i>
   </a>
 
-  <a href="#" style="width:12.5%">
-    <i class="likesitem la la-share fa-2x" style="width:100%"></i>
+  {{-- si estas logueado --}}
+  @if($post->user->id == Auth::user()->id)
+  <a href="/edit/{{$post->id}}" style="width:12.5%">
+    <i class="las la-edit fa-2x" style="width:100%"></i>
   </a>
 
-  <a href="#" style="width:12.5%">
-    <i class="likesitem la la-ellipsis-v fa-2x" style="width:100%"></i>
+  <a href="/destroy/{{$post->id}}" style="width:12.5%">
+    <i class="las la-trash-alt fa-2x" style="width:100%"></i>>
   </a>
+
+@endif
 </div>
